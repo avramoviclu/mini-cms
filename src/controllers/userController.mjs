c7d036dac2503e5e3289fd81c6f3e5c123f1b74d
Luka Avramovic <avramoviclu@gmail.com>
2024-01-15 19:01:24 +0100

    Adds userService.

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -1,18 +1,36 @@
-const getUsers = ((request, response) => {});
+import * as userService from "../services/userService.mjs";
 
-const getUser = ((request, response) => {});
+const getUsers = ((request, response) => {
+    return userService.getUsers(request, response);
+});
 
-const createUser = ((request, response) => {});
+const getUser = ((request, response) => {
+    return userService.getUser(request, response);
+});
 
-const updateUser = ((request, response) => {});
+const createUser = ((request, response) => {
+    return userService.createUser(request, response);
+});
 
-const deleteUser = ((request, response) => {});
+const updateUser = ((request, response) => {
+    return userService.updateUser(request, response);
+});
 
-const acceptInvitation = ((request, response) => {});
+const deleteUser = ((request, response) => {
+    return userService.deleteUser(request, response);
+});
 
-const getInvitation = ((request, response) => {});
+const acceptInvitation = ((request, response) => {
+    return userService.acceptInvitation(request, response);
+});
 
-const createInvitation = ((request, response) => {});
+const getInvitation = ((request, response) => {
+    return userService.getInvitation(request, response);
+});
+
+const createInvitation = ((request, response) => {
+    return userService.createInvitation(request, response);
+});
 
 module.exports = {
     getUsers,
---
+++ src/services/userService.mjs
@@ -0,0 +1,26 @@
+const getUsers = ((request, response) => {});
+
+const getUser = ((request, response) => {});
+
+const createUser = ((request, response) => {});
+
+const updateUser = ((request, response) => {});
+
+const deleteUser = ((request, response) => {});
+
+const acceptInvitation = ((request, response) => {});
+
+const getInvitation = ((request, response) => {});
+
+const createInvitation = ((request, response) => {});
+
+module.exports = {
+    getUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    acceptInvitation,
+    getInvitation,
+    createInvitation
+}
