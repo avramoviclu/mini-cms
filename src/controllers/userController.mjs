bd2a3496cfb13ffed394cc317b59541fc3daacfe
Luka Avramovic <avramoviclu@gmail.com>
2024-01-25 18:30:58 +0100

    Refactoring, converts into class.

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -1,44 +1,37 @@
-import * as userService from "../services/userService.mjs";
+import userService from "../services/userService.mjs";
 
-const getUsers = ((request, response) => {
-    return userService.getUsers(request, response);
-});
+class UserController {
+    static getUsers(request, response) {
+        return userService.getUsers(request, response);
+    }
 
-const getUser = ((request, response) => {
-    return userService.getUser(request, response);
-});
+    static getUser(request, response) {
+        return userService.getUser(request, response);
+    }
 
-const createUser = ((request, response) => {
-    return userService.createUser(request, response);
-});
+    static createUser(request, response) {
+        return userService.createUser(request, response);
+    }
 
-const updateUser = ((request, response) => {
-    return userService.updateUser(request, response);
-});
+    static updateUser(request, response) {
+        return userService.updateUser(request, response);
+    }
 
-const deleteUser = ((request, response) => {
-    return userService.deleteUser(request, response);
-});
+    static deleteUser(request, response) {
+        return userService.deleteUser(request, response);
+    }
 
-const acceptInvitation = ((request, response) => {
-    return userService.acceptInvitation(request, response);
-});
+    static acceptInvitation(request, response) {
+        return userService.acceptInvitation(request, response);
+    }
 
-const getInvitation = ((request, response) => {
-    return userService.getInvitation(request, response);
-});
+    static getInvitation(request, response) {
+        return userService.getInvitation(request, response);
+    }
 
-const createInvitation = ((request, response) => {
-    return userService.createInvitation(request, response);
-});
-
-module.exports = {
-    getUsers,
-    getUser,
-    createUser,
-    updateUser,
-    deleteUser,
-    acceptInvitation,
-    getInvitation,
-    createInvitation
+    static createInvitation(request, response) {
+        return userService.createInvitation(request, response);
+    }
 }
+
+export default UserController;
