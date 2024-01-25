1a19474f72a35ab9b860fd443773ac40afe78bc5
Luka Avramovic <avramoviclu@gmail.com>
2024-01-25 18:29:14 +0100

    Refactoring, converts into class.

--- src/controllers/postController.mjs
+++ src/controllers/postController.mjs
@@ -1,29 +1,25 @@
-import * as postService from "../services/postService.mjs";
+import postService from "../services/postService.mjs";
 
-const getPosts = ((request, response) => {
-    return postService.getPosts(request, response);
-});
+class PostController {
+    static getPosts(request, response) {
+        return postService.getPosts(request, response);
+    }
 
-const getPost = ((request, response) => {
-    return postService.getPost(request, response);
-});
+    static getPost(request, response) {
+        return postService.getPost(request, response);
+    }
 
-const createPost = ((request, response) => {
-    return postService.createPost(request, response);
-});
+    static createPost(request, response) {
+        return postService.createPost(request, response);
+    }
 
-const updatePost = ((request, response) => {
-    return postService.updatePost(request, response);
-});
+    static updatePost(request, response) {
+        return postService.updatePost(request, response);
+    }
 
-const deletePost = ((request, response) => {
-    return postService.deletePost(request, response);
-});
-
-module.exports = {
-    getPosts,
-    getPost,
-    createPost,
-    updatePost,
-    deletePost
+    static deletePost(request, response) {
+        return postService.deletePost(request, response);
+    }
 }
+
+export default PostController;
