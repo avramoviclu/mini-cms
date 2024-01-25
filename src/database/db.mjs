e6e57403bbb45d6f5de18c32e236c3630300d7e2
Luka Avramovic <avramoviclu@gmail.com>
2024-01-25 18:42:55 +0100

    Refactoring, updates Db class.

--- src/database/db.mjs
+++ src/database/db.mjs
@@ -1,15 +1,16 @@
 import pg from "pg";
-
 const { Pool } = pg;
 
-const pool = new Pool({
-    user: process.env.DB_USERNAME,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-});
-
-module.exports = {
-    pool
+class Db {
+    pool = () => {
+        return new Pool({
+            user: process.env.DB_USERNAME,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT
+        });
+    }
 }
+
+export default Db;
