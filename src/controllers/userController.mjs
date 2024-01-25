d44e03d576f411b00f50d23c186f88111b9a0c7a
Luka Avramovic <avramoviclu@gmail.com>
2024-01-25 18:46:01 +0100

    Refactoring, converts to arrow function.

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -1,35 +1,35 @@
 import UserService from "../services/userService.mjs";
 
 class UserController {
-    static getUsers(request, response) {
+    getUsers = (request, response) => {
         return UserService.getUsers(request, response);
     }
 
-    static getUser(request, response) {
+    getUser = (request, response) => {
         return UserService.getUser(request, response);
     }
 
-    static createUser(request, response) {
+    createUser = (request, response) => {
         return UserService.createUser(request, response);
     }
 
-    static updateUser(request, response) {
+    updateUser = (request, response) => {
         return UserService.updateUser(request, response);
     }
 
-    static deleteUser(request, response) {
+    deleteUser = (request, response) => {
         return UserService.deleteUser(request, response);
     }
 
-    static acceptInvitation(request, response) {
+    acceptInvitation = (request, response) => {
         return userService.acceptInvitation(request, response);
     }
 
-    static getInvitation(request, response) {
+    getInvitation = (request, response) => {
         return UserService.getInvitation(request, response);
     }
 
-    static createInvitation(request, response) {
+    createInvitation = (request, response) => {
         return UserService.createInvitation(request, response);
     }
 }
