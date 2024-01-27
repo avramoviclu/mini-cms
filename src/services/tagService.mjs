ffe38b9319cf244aba2b96f398ad9c5026d3e935
Luka Avramovic <avramoviclu@gmail.com>
2024-01-27 17:49:35 +0100

    Adds Tag service.

---
+++ src/services/tagService.mjs
@@ -0,0 +1,23 @@
+class TagService {
+    getTags = (request, response) => {
+        //TODO: Finish method implementation        
+    };
+
+    getTag = (request, response) => {
+        //TODO: Finish method implementation
+    };
+
+    createTag = (request, response) => {
+        //TODO: Finish method implementation
+    };
+
+    updateTag = (request, response) => {
+        //TODO: Finish method implementation
+    };
+
+    deleteTag = (request, response) => {
+        //TODO: Finish method implementation
+    };
+}
+
+export default TagService;
