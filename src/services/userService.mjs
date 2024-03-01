b90e6fd5f28c2f6f2b8e185137402403b98702da
Luka Avramovic <avramoviclu@gmail.com>
2024-03-01 23:03:10 +0100

    Refactoring user service.

--- src/services/userService.mjs
+++ src/services/userService.mjs
@@ -1,35 +1,64 @@
+import db from '../database/db.mjs';
+
 class UserService {
-    getUsers = (request, response) => {
-        //TODO: Finish method implementation        
-    };
+    static getUsers() {
+        return new Promise((resolve, reject) => {
+            db.all("SELECT * FROM users", [], (err, rows) => {
+                if (err) reject(err);
+                else resolve(rows);
+            });
+        });
+    }
 
-    getUser = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static getUser(id) {
+        return new Promise((resolve, reject) => {
+            db.get("SELECT * FROM users WHERE id = ?", [id], (err, row) => {
+                if (err) reject(err);
+                else resolve(row);
+            });
+        });
+    }
 
-    createUser = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static createUser(userData) {
+        return new Promise((resolve, reject) => {
+            const { title, content } = tagData;
+            db.run("INSERT INTO users (title, content) VALUES (?, ?)", [title, content], function(err) {
+                if (err) reject(err);
+                else resolve({ id: this.lastID, ...postData });
+            });
+        });
+    }
 
-    updateUser = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static updateUser(id, tagData) {
+        return new Promise((resolve, reject) => {
+            const { title, content } = postData;
+            db.run("UPDATE users SET title = ?, content = ? WHERE id = ?", [title, content, id], function(err) {
+                if (err) reject(err);
+                else resolve({ id, ...postData });
+            });
+        });
+    }
 
-    deleteUser = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static deleteUser(id) {
+        return new Promise((resolve, reject) => {
+            db.run("DELETE FROM users WHERE id = ?", [id], function(err) {
+                if (err) reject(err);
+                else resolve({ message: 'User deleted successfully' });
+            });
+        });
+    }
 
-    acceptInvitation = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static acceptInvitation() {
+        // TODO: implementation
+    }
 
-    getInvitation = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static getInvitation() {
+        // TODO: implementation
+    }
 
-    createInvitation = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static createInvitation() {
+        // TODO: implementation
+    }
 }
 
 export default UserService;
