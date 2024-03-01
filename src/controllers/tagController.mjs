cfe3d86cf41967000b64f506c156fae2c705553e
Luka Avramovic <avramoviclu@gmail.com>
2024-03-01 22:56:04 +0100

    Refactoring tag controller and service.

--- src/controllers/tagController.mjs
+++ src/controllers/tagController.mjs
@@ -1,24 +1,53 @@
-import TagService from "../services/tagService.mjs";
+import TagService from '../services/TagService.mjs';
 
 class TagController {
-    getTags = (request, response) => {
-        return TagService.getTags(request, response);
+    static getTags = async (request, response) => {
+        try {
+            const tags = await TagService.getTags();
+            response.json(tags);
+        } catch (error) {
+            response.status(500).json({ message: error.message });
+        }
     }
 
-    getTag = (request, response) => {
-        return TagService.getTag(request, response);
+    static getTag = async (request, response) => {
+        try {
+            const tag = await TagService.getTag(request.params.id);
+            if (tag) {
+                response.json(tag);
+            } else {
+                response.status(404).json({ message: 'Tag not found' });
+            }
+        } catch (error) {
+            response.status(500).json({ message: error.message });
+        }
     }
 
-    createTag = (request, response) => {
-        return TagService.createTag(request, response);
+    static createTag = async (request, response) => {
+        try {
+            const newTag = await TagService.createTag(request.body);
+            response.status(201).json(newTag);
+        } catch (error) {
+            response.status(400).json({ message: error.message });
+        }
     }
 
-    updateTag = (request, response) => {
-        return TagService.updateTag(request, response);
+    static updatePost = async (request, response) => {
+        try {
+            const updatedTag = await TagService.updateTag(request.params.id, request.body);
+            response.json(updatedTag);
+        } catch (error) {
+            response.status(400).json({ message: error.message });
+        }
     }
 
-    deleteTag = (request, response) => {
-        return TagService.deleteTag(request, response);
+    static deleteTag = async (request, response) => {
+        try {
+            await TagService.deleteTag(request.params.id);
+            response.json({ message: 'Tag successfully deleted' });
+        } catch (error) {
+            response.status(500).json({ message: error.message });
+        }
     }
 }
 
--- src/services/tagService.mjs
+++ src/services/tagService.mjs
@@ -1,23 +1,52 @@
+import db from '../database/db.mjs';
+
 class TagService {
-    getTags = (request, response) => {
-        //TODO: Finish method implementation        
-    };
+    static getTags() {
+        return new Promise((resolve, reject) => {
+            db.all("SELECT * FROM tags", [], (err, rows) => {
+                if (err) reject(err);
+                else resolve(rows);
+            });
+        });
+    }
 
-    getTag = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static getTag(id) {
+        return new Promise((resolve, reject) => {
+            db.get("SELECT * FROM tags WHERE id = ?", [id], (err, row) => {
+                if (err) reject(err);
+                else resolve(row);
+            });
+        });
+    }
 
-    createTag = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static createTag(tagData) {
+        return new Promise((resolve, reject) => {
+            const { title, content } = tagData;
+            db.run("INSERT INTO tags (title, content) VALUES (?, ?)", [title, content], function(err) {
+                if (err) reject(err);
+                else resolve({ id: this.lastID, ...postData });
+            });
+        });
+    }
 
-    updateTag = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static updateTag(id, tagData) {
+        return new Promise((resolve, reject) => {
+            const { title, content } = postData;
+            db.run("UPDATE tags SET title = ?, content = ? WHERE id = ?", [title, content, id], function(err) {
+                if (err) reject(err);
+                else resolve({ id, ...postData });
+            });
+        });
+    }
 
-    deleteTag = (request, response) => {
-        //TODO: Finish method implementation
-    };
+    static deleteTag(id) {
+        return new Promise((resolve, reject) => {
+            db.run("DELETE FROM tags WHERE id = ?", [id], function(err) {
+                if (err) reject(err);
+                else resolve({ message: 'Tag deleted successfully' });
+            });
+        });
+    }
 }
 
-export default TagService;
+export default PostService;
