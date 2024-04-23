92f26e1bbc39581e8c36b05ed81350cb30dd84f0
Luka Avramovic <avramoviclu@gmail.com>
2024-04-23 21:05:49 +0200

    Updates db script.

--- src/database/db.mjs
+++ src/database/db.mjs
@@ -12,6 +12,23 @@ const db = new (verbose().Database)('./sqlite/mini-cms.db', (err) => {
             title TEXT NOT NULL,
             content TEXT NOT NULL
         )`);
+
+        db.run(`CREATE TABLE IF NOT EXISTS tags (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT NOT NULL
+        )`);
+
+        db.run(`CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL
+        )`);
+
+        db.run(`CREATE TABLE IF NOT EXISTS post_tag (
+            post_id INTEGER,
+            tag_id INTEGER
+        )`);
     }
 });
 
